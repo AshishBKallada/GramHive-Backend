e5fd1a078d2af623e16346d04907bdc782cd2ad4
ASHISH B KALLADA <ashishbkallada@gmail.com>
2024-06-28 16:11:35 +0530

    checking file share bug

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ const morgan = require('morgan');
 const app = express();
 const port = config.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cors({ origin: config.ORIGIN, credentials: true, preflightContinue: true }));
 app.use(helmet({ xssFilter: true }));
 app.use(ExpressMongoSanitize());
@@ -26,7 +26,7 @@ app.use(morgan('dev'));
 // app.use(rateLimiter);
 app.use(errorHandler);
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 connectToMongoDB();
 
